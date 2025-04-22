3a5cc19b8d6ff3f50f98de5ad622a6de4432074a
404Setup <153366651+404Setup@users.noreply.github.com>
2025-04-22 15:30:56 +0800

    add

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "t-i18n"
---
+++ src/main/java/one/tranic/t/i18n/I18nLoader.java
@@ -0,0 +1,9 @@
+package one.tranic.t.i18n;
+
+import java.util.Locale;
+import java.util.Map;
+
+public class I18nLoader {
+    Map<String, String> language;
+    Locale locale;
+}
