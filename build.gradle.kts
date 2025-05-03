ba65bca78d68b63508f5cadec3e21c78f101e071
404Setup <153366651+404Setup@users.noreply.github.com>
2025-05-03 23:42:26 +0800

    bump dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     compileOnly("org.yaml:snakeyaml:2.4")
     compileOnly("com.google.code.gson:gson:2.13.0")
 
-    compileOnly("one.tranic:t-utils:1.2.2.1")
+    compileOnly("one.tranic:t-utils:1.2.3")
 
     compileOnly("org.jetbrains:annotations:24.1.0")
 }
